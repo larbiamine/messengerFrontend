43e3a0669bbae08de9f106287f11dd96eb47bb5c
larbiamine <larbiaminelj@gmail.com>
2023-02-06 15:12:20 +0100

    Added redux to manage user State  [#4]

---
+++ src/redux/userRedux.ts
@@ -0,0 +1,72 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { initialState } from "../authTypes";
+
+const iState: initialState = {
+	currentUser: null,
+	isFetching: false,
+	isRegisterError: false,
+	isLoginError: false,
+	registerError: "",
+};
+
+const userSlice = createSlice({
+	name: "user",
+	initialState: iState,
+	reducers: {
+		initState: (state) => {
+			state.isFetching = false;
+			state.registerError = "";
+			state.isLoginError = false;
+			state.isRegisterError = false;
+		},
+		loginStart: (state) => {
+			state.isFetching = true;
+		},
+
+		loginSuccess: (state, action) => {
+			state.isFetching = false;
+			state.isLoginError = false;
+			state.currentUser = action.payload;
+		},
+
+		logout: (state) => {
+			state.currentUser = null;
+			state.isLoginError = false;
+
+			state.isFetching = false;
+			state.registerError = "";
+
+			state.isRegisterError = false;
+		},
+		loginFailed: (state) => {
+			state.isLoginError = true;
+			state.isFetching = false;
+		},
+		registerStart: (state) => {
+			state.isFetching = true;
+		},
+		registerSuccess: (state) => {
+			state.isFetching = false;
+			state.isRegisterError = false;
+		},
+
+		registerFailed: (state, action) => {
+			state.registerError = action.payload;
+			state.isRegisterError = true;
+			state.isFetching = false;
+		},
+	},
+});
+
+export const {
+	registerStart,
+	registerSuccess,
+	registerFailed,
+	loginFailed,
+	logout,
+	loginSuccess,
+	loginStart,
+	initState,
+} = userSlice.actions;
+
+export default userSlice.reducer;
